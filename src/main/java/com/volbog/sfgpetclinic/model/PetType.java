393168a6c8fd866666fd186fc0964cf8cb77e129
VolBog <volbog93@gmail.com>
2022-12-15 15:07:04 +0100

    adding PetType PoJO

---
+++ src/main/java/com/volbog/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.volbog.sfgpetclinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PetType {
+
+    private String name;
+
+
+}
