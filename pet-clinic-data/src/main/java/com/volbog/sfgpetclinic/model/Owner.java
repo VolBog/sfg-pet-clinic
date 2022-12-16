bca7ba2c8dcf10512751462e43bfa50758338a4f
VolBog <volbog93@gmail.com>
2022-12-16 09:24:32 +0100

    Prepare for first release

---
+++ pet-clinic-data/src/main/java/com/volbog/sfgpetclinic/model/Owner.java
@@ -0,0 +1,9 @@
+package com.volbog.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Owner extends Person {
+}
---
+++ pet-clinic-data/src/main/java/com/volbog/sfgpetclinic/model/Pet.java
@@ -0,0 +1,20 @@
+package com.volbog.sfgpetclinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+
+}
